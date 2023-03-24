a5d06e2206f34105f474c484730da2af5dce01c1
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2023-03-24 09:01:27 -0300

    Added Correct game logic - if the blank tile is not near the one the player clicked, it wont be moved. Added win condition

--- script.js
+++ script.js
@@ -8,45 +8,34 @@ let correctPositions = [
 ];
 let re = /[0-9]/;
 let allTiles = document.querySelectorAll(".puzzle div:not(.col)");
-let currentPositions = Array.from(allTiles, (tile) => tile.id);
-let currentPositions2d = [];
-//set num of rows and columns
 const numOfRows = 3;
-//making the array 2d
-// for (let row = 0 ; row<numOfRows;row++)
-//   {
-//     currentPositions2d[row]=[]
 
-//     for (let col = 0; col <numOfRows ; col++)
-//       {
+let initialPositions2d = getPositions();
+console.log(`the initial positions are`, initialPositions2d);
 
-//         currentPositions2d[row][col]=currentPositions[(row*3)+col]
-//       }
-//   }
-console.log(currentPositions);
-console.log(currentPositions2d);
-
+//adding click event
 for (colorTile of colorTiles) {
   colorTile.addEventListener("click", (event) => {
     let clickedTile = event.target;
-    console.log(event.target.getAttribute("data-color"));
     let clickedTileColor = event.target.getAttribute("data-color");
     let blankTile = document.querySelector("div[data-color='gray']");
-
+    let currentPositions2d = getPositions();
     console.log(blankTile.getAttribute("data-color"));
-    checkIfMoveable(blankTile.id, currentPositions2d);
-    //swapping colors
-    event.target.setAttribute("data-color", "gray");
-    blankTile.setAttribute("data-color", clickedTileColor);
-    checkPositions();
-    changeIds(clickedTile, blankTile);
-    console.log(checkPositions(currentPositions));
+    if (checkIfNext(blankTile.id, clickedTile.id, currentPositions2d)) {
+      //swapping colors
+      event.target.setAttribute("data-color", "gray");
+      blankTile.setAttribute("data-color", clickedTileColor);
+      getPositions();
+      changeIds(clickedTile, blankTile);
+    }
+    checkWinCondition(getPositions(), correctPositions);
   });
   // console.log(colorTile.getAttribute("data-color"))
 }
 
-function checkPositions(currentPositions) {
-  currentPositions = Array.from(allTiles, (tile) => tile.id);
+function getPositions() {
+  let currentPositions = Array.from(allTiles, (tile) => tile.id);
+  let currentPositions2d = [];
   //making the array 2d
   for (let row = 0; row < numOfRows; row++) {
     currentPositions2d[row] = [];
@@ -55,6 +44,7 @@ function checkPositions(currentPositions) {
       currentPositions2d[row][col] = currentPositions[row * 3 + col];
     }
   }
+  console.log("currentPositions2d", currentPositions2d);
   return currentPositions2d;
 }
 
@@ -67,21 +57,37 @@ function changeIds(clickedTile, blankTile) {
   blankTile.textContent = placeholderText;
 }
 
-function checkIfMoveable(blankTileId, currentPositions2d) {
-  let index = getIndexBlankTile(blankTileId, currentPositions2d);
-  let currentPosition = checkPositions(currentPositions);
-  console.log(index);
+function checkIfNext(blankTileId, clickedTileId, currentPositions) {
+  let indexBlankTile = getTileIndex(blankTileId, currentPositions);
+  let indexClickedTile = getTileIndex(clickedTileId, currentPositions);
+  let [xB, yB] = indexBlankTile;
+  let [xC, yC] = indexClickedTile;
+
+  console.log("index of blank tile", indexBlankTile);
+  console.log("index of clicked tile", indexClickedTile);
+  if (
+    (xC === xB && yC === yB + 1) ||
+    (xC === xB && yC === yB - 1) ||
+    (xC === xB + 1 && yC === yB) ||
+    (xC === xB - 1 && yC === yB)
+  ) {
+    console.log("tile is next to blank tile and can be moved");
+    return true;
+  } else {
+    console.log("tile cant be moved");
+    return false;
+  }
   //I need to find the index of the id in currentPosition array to check
-  // the Ids that are close enough to be moveable
+  // the Ids that are close enough to be moveable this means
 }
-function getIndexBlankTile(blankTileId, currentPositions2d) {
+function getTileIndex(TileId, currentPositions2d) {
   let x;
   let y;
   outer_loop: for (let r = 0; r < currentPositions2d.length; r++) {
     console.log(currentPositions2d[r]);
     for (let c = 0; c < currentPositions2d.length; c++) {
       console.log(currentPositions2d[r][c]);
-      if (blankTileId !== currentPositions2d[r][c]) {
+      if (TileId !== currentPositions2d[r][c]) {
         continue;
       }
       console.log(r, c);
@@ -98,3 +104,22 @@ function getIndexBlankTile(blankTileId, currentPositions2d) {
 //   initBlankTile.classList.remove("grey")
 //   initBlankTile.classList.add("yellow")
 // })
+
+function checkWinCondition(currentTilesPositions, correctPositions) {
+  console.log(currentTilesPositions.join());
+  console.log(correctPositions.join());
+
+  if (currentTilesPositions.join() === correctPositions.join()) {
+    console.log("game won");
+    alert("game won");
+    //TODO remover as bordas
+    //TODOremover o clique
+    return true;
+  } else {
+    console.log("game not won yet");
+    return false;
+  }
+}
+
+//TODO Randomize tile positions
+//TODO check if game is winnable
